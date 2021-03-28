382d420d55444f8511ea74002cf1c34e6270aaf5
chrisQua3od <christengeorge25@gmail.com>
2021-03-28 20:24:42 +0200

    books and authors pages

--- src/app/components/authors/authors.component.ts
+++ src/app/components/authors/authors.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { PaginationService } from 'src/app/services/pagination.service'
 
 @Component({
   selector: 'app-authors',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./authors.component.css']
 })
 export class AuthorsComponent implements OnInit {
-
-  constructor() { }
+  data:Array<any>=[]
+  totalRecords:string = ''
+  page:number=1
+  constructor(private paginate:PaginationService) { 
+    this.data = new Array<any>()
+  }
 
   ngOnInit(): void {
+    this.paginate.getData().subscribe((data)=>{
+      this.data = data.results
+      this.totalRecords = data.results.length
+      console.log(this.data)
+    })
   }
+  @Input() allBookslist:Array<{img:string,authName:string,avgRate:number,myRate:number}> = []
 
 }
--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { PaginationService } from 'src/app/services/pagination.service'
 
 @Component({
   selector: 'app-books',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-
-  constructor() { }
+  data:Array<any>=[]
+  totalRecords:string = ''
+  page:number=1
+  constructor(private paginate:PaginationService) { 
+    this.data = new Array<any>()
+  }
 
   ngOnInit(): void {
+    this.paginate.getData().subscribe((data)=>{
+      this.data = data.results
+      this.totalRecords = data.results.length
+      console.log(this.data)
+    })
+
   }
+  @Input() allBookslist:Array<{img:string,authName:string,avgRate:number,myRate:number}> = []
 
 }
