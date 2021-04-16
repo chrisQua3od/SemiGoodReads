c912dc9c2c00fd8fbc4c6e1a3bfd529b54369460
AyaHassan94 <ayah7386@gmail.com>
2021-04-16 20:53:02 +0200

    DB updated

--- NodeJs/myapp/models/categories.js
+++ NodeJs/myapp/models/categories.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const categoriesSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    // categoryId: { type: Number, required: true },
+    name: { type: String, required: true  , unique:true},
+    
     books: [{ type: mongoose.Schema.Types.ObjectId, ref: 'books' }]
 })
 
--- NodeJs/myapp/models/users.js
+++ NodeJs/myapp/models/users.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
     photo: { type: String },
     fname: { type: String, required: true },
     lname: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true , unique : true },
     password: { type: String, required: true },
     library: [
         {
